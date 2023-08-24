6e6e6e05117823f0281de7ff28e3d30d06ffa1d0
Hadi Ahmad <hadiahmad676@gmail.com>
2023-08-24 12:46:48 -0700

    0-print_lists.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,19 +11,14 @@ size_t print_list(const list_t *h)
 
 	if (h == NULL)
 		return (0);
-
 	for (i = 1; h->next != NULL; i++)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%u] %s\n", h->len, "(nil)");
-		}
 		else
-		{
 			printf("[%u) %s\n", h->len, h->str);
-		}
 		h = h->next;
 	}
-	printf("[%u] %s\n", h->len, "(nil)");
+	printf("[%u] %s\n", h->len, h->str);
 	return (i);
 }
