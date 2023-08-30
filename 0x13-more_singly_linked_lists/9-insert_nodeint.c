1855f4dde067b25cf094f1cfcf82eb870965cecd
Hadi Ahmad <hadiahmad676@gmail.com>
2023-08-29 18:52:53 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *tmp;
 	listint_t *tmp_old;
-	listint_t * new_node;
+	listint_t *new_node;
 
 	tmp = *head;
 	if (head == NULL)
@@ -45,7 +45,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		*head = new_node;
 	}
-	for (i = 0; i < idx -1 && tmp != NULL && idx != 0; i++)
+	for (i = 0; i < idx - 1 && tmp != NULL && idx != 0; i++)
 		tmp = tmp->next;
 	if (tmp == NULL)
 		return (NULL);
