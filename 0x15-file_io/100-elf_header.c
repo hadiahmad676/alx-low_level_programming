e8daae83a495d1f5b159b5b34f5ca23885622fdb
Hadi Ahmad <hadiahmad676@gmail.com>
2023-09-04 16:59:58 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,4 +5,47 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
+/**
+ * print_addr - prints address
+ * @ptr: magic
+ * Return: no return
+ */
+void print_addr(char *ptr)
+{
+	int i;
+	int start;
+	char sys;
 
+	printf("  Entry point address:               0x");
+
+	sys = ptr[4] + '0';
+	if (sys == '1')
+	{
+		start = 26;
+		printf("80");
+		for (i = begin; i >= 22; i--)
+		{
+			if (ptr[i] > 0)
+				printf("%x", ptr[i]);
+			else if (ptr[i] < 0)
+				printf("%x", 256 + ptr[i]);
+		}
+		if (ptr[7] == 6)
+			printf("00");
+	}
+
+	if (sys == '2')
+	{
+		start = 26;
+		for (i = start; i > 23; i--)
+		{
+			if (ptr[i] >= 0)
+				printf("%02x", ptr[i]);
+
+			else if (ptr[i] < 0)
+				printf("%02x", 256 + ptr[i]);
+
+		}
+	}
+	printf("\n");
+}
